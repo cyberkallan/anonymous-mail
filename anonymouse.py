0e803c5001c04361946c910dac5d32ff0f3678c2
Cyber kallan <56509491+cyberkallan@users.noreply.github.com>
2019-11-18 12:03:48 +0530

    Update anonymouse.py

--- anonymouse.py
+++ anonymouse.py
@@ -1,34 +1,35 @@
 
 
 
+coded by CYBER KALLAN (Mallu Youtuber)
+
+	
 import requests
 
-print("\nSms bombing sytem by cyber kallan")
+print("\nAnonymous Email by cyber kallan")
 print("coded by Arjun arz (mallu youtuber) \n")
 to = raw_input('to: ')
+subject = raw_input('subject: ')
+message = raw_input('message: ')
 
-
-
-user_agent = ':Mozilla/5.0 (Linux; Android 8.1.0; vivo 1718) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 sess = requests.Session()
-email_req = sess.post('http://accounts.justdial.com/hr_dashboard_services/jdtest/sendsms_registration', headers={
-	'Host': 'accounts.justdial.com',
+email_req = sess.post('http://anonymouse.org/cgi-bin/anon-email.cgi', headers={
+	'Host': 'anonymouse.org',
 	'User-Agent': user_agent,
-	'Accept': 'application/json, text/javascript, */*; q=0.01',
-	'Accept-Language': 'en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7,hi;q=0.6,ar;q=0.5',
-	
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	'Accept-Language': 'en-US,en;q=0.5',
 	'Accept-Encoding': 'gzip, deflate',
-	'Referer': 'http://accounts.justdial.com/hr_dashboard_services/jdtest/register?mobile=NzYyMTA1Nzk0MQ==&id=MC4zMTU5MDkwMCAxNTY4NzAxODUyfEB8K%2FlonurGfX0%3D&city=',
+	'Referer': 'http://anonymouse.org/anonemail.html',
         'Connection': 'close',
         'Upgrade-Insecure-Requests':'1',
-        'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+        'Content-Type':'application/x-www-form-urlencoded'
 }, data={
 	'to': to,
-	
-	
+	'subject': subject,
+	'text': message
 })
 
-if 'The sms has been sended successfully' in email_req.text:
-    print("[+] SMS Sent!")
-    print("[+] In order to increase your privacy, the cyber kallan spam system will be randomly delayed up to 10 minutes")
-
+if 'The e-mail has been sent' in email_req.text:
+    print("[+] Email Sent!")
+    print("[+] In order to increase your privacy, the anonymous e-mail will be randomly delayed up to 12 hours")
